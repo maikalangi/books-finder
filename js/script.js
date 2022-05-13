f21b32c0e6b78fc711a679ed14ecdeb61a979d0c
maikalangi <maikalangi@gmail.com>
2022-05-12 23:53:51 -0700

    commits new additions to css: shadows, font styles, placements, sizings. effects

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-// variables
+// VARIABLES
 const url = 'https://www.googleapis.com/books/v1/volumes?q=';
 const key = '&key=AIzaSyCeRKMon4_VVEskljoG-PvfJOPbse5qIPY'
 
-// element references
+// ELEMENT REFERENCES
 const $cover = $('img');
 const $title = $('#title');
 const $author = $('#author');
@@ -12,38 +12,36 @@ const $form = $('form');
 const $input = $('input[type="text"]');
 const $section = $('#results');
 
-// event listeners
+// EVENT LISTENERS
 $form.on('submit', handleGetData);
 
-// functions
+// FUNCTIONS
 function handleGetData(event) {
 
     event.preventDefault();
     const userInput = $input.val();
 
     $.ajax(url + userInput + key + '&maxResults=40').then((data) => {
+        
         const axs = data.items.map(more=>more.volumeInfo);
-        // console.log(axs);
         const arr = [];
         
         $(axs).each(i=>{
-            // console.log(axs[i]);
+            
             const $result = $('<img>');
-            // const $wrap = $(`<a href='${axs[i]}'>`);
 
-            // $section.append
-            // ($wrap.attr('href', axs[i]))
-
-            // $result.wrap($wrap);
             $section.append
             ($result.attr('src', axs[i].imageLinks.thumbnail));
+                
                 $result.on('click', ()=>{
                     console.log(axs[i]);
+                    
                     $title.text(axs[i].title);
                     $author.text(axs[i].authors);
                     $date.text(axs[i].publishedDate);
                     $summary.text(axs[i].description);
                     $('img').attribute('src', `${axs[i].imageLinks.thumbnail}`);
+                    
                     }
                 )
             }
